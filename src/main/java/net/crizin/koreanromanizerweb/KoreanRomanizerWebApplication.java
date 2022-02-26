c10dd19227eb15f1895901c2175d917258d3e444
JaeYong Lee <crizin@gmail.com>
2022-02-26 19:02:17 +0900

    .

--- src/main/java/net/crizin/koreanromanizerweb/KoreanRomanizerWebApplication.java
+++ src/main/java/net/crizin/koreanromanizerweb/KoreanRomanizerWebApplication.java
@@ -1,30 +1,34 @@
 package net.crizin.koreanromanizerweb;
 
+import java.util.Optional;
 import net.crizin.KoreanCharacter;
 import net.crizin.KoreanRomanizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class KoreanRomanizerWebApplication {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
-
-	@ResponseBody
-	@PostMapping("/")
-	public String romanize(@RequestParam String string,
+	@RequestMapping("/")
+	public String index(Model model,
+			@RequestParam(required = false) String string,
 			@RequestParam(required = false) KoreanCharacter.Type type,
 			@RequestParam(required = false) KoreanCharacter.ConsonantAssimilation consonantAssimilation) {
-		return KoreanRomanizer.romanize(string, type, consonantAssimilation);
+
+		model.addAttribute("type", Optional.ofNullable(type).map(Enum::name).orElse(null));
+		model.addAttribute("consonantAssimilation", Optional.ofNullable(consonantAssimilation).map(Enum::name).orElse(null));
+
+		if (string != null && !string.isEmpty()) {
+			model.addAttribute("string", string);
+			model.addAttribute("romanized", KoreanRomanizer.romanize(string, type, consonantAssimilation));
+		}
+
+		return "index";
 	}
 
 	public static void main(String[] args) {
