c96fe0ea7311541f3c6476e451dad1e3f1ff23f3
JaeYong Lee <crizin@gmail.com>
2022-02-26 11:22:04 +0900

    Initial commit

---
+++ src/test/java/net/crizin/koreanromanizerweb/KoreanRomanizerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.crizin.koreanromanizerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanRomanizerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
